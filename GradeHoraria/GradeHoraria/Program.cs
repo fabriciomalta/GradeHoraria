1eccaa8851a3aea5aac25d345941eeb7fac3be5e
Luiz Chun <53568746+luizch98@users.noreply.github.com>
2021-12-16 21:34:37 -0300

    Inicial
    Solução Inicial

---
+++ GradeHoraria/GradeHoraria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradeHoraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
